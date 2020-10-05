743357bf7967b0ca45d5d5f547082fe5f943e100
Vadim7007 <vadim-baz@mail.ru>
2020-10-05 11:28:45 +0300

    lab2+test

--- 2/lab2.cpp
+++ 2/lab2.cpp
@@ -9,45 +9,40 @@ Tr::Tr(double p) {
 	}
 }
 
-void Tr::set(double new_p) {
+Tr Tr::set(double new_p) {
 	if (new_p > 0) {
 		a = new_p;
 	}
 	else {
 		throw std::exception("Negative parametr.");
 	}
+	return *this;
 }
 
 void Tr::coord(double& x, double& y, double n_pi) const {
-	const double pi = 3.141592;
 	n_pi = abs(n_pi) - trunc(abs(n_pi));
 	y = a * sin(n_pi * pi);
 	x = a * cos(n_pi * pi) + a * log(tan(n_pi * pi / 2));
 }
 
 double Tr::len(double n_pi) const {
-	const double pi = 3.141592;
 	n_pi = abs(n_pi) - trunc(abs(n_pi));
 	return a * log(1 / sin(n_pi * pi));
 }
 
 double Tr::rad(double n_pi) const {
-	const double pi = 3.141592;
 	n_pi = abs(n_pi) - trunc(abs(n_pi));
 	return (a / tan(n_pi * pi));
 }
 
 double Tr::area(void) const {
-	const double pi = 3.141592;
 	return (pi * a * a / 2);
 }
 
 double Tr::surface(void) const {
-	const double pi = 3.141592;
 	return (pi * a * a * 4);
 }
 
 double Tr::volume(void) const {
-	const double pi = 3.141592;
 	return (pi * a * a * a * 2 / 3);
 }
--- 2/lab2.h
+++ 2/lab2.h
@@ -4,17 +4,17 @@ class Tr	// трактрисса
 {
 public:
 	Tr() {
-		a = 1;
+		set(1);
 	};				// конструктор
 	Tr(double p);					// конструктор
-	~Tr() {};						// деструктор
-	void set(double new_p);			// изменение параметра кривой
+	Tr set(double new_p);			// изменение параметра кривой
 	
 	double get(void) const {		// получение параметра кривой
 		return a;
 	};
 	void coord(double& x, 
-	double& y, double n_pi) const;	// получение координат по углу
+			   double& y, 
+			   double n_pi) const;	// получение координат по углу
 	double len(double n_pi) const;		// возвращает длину дуги
 	double rad(double n_pi) const;		// радиус кривизны
 	double area(void) const;			// площадь полосы
@@ -22,4 +22,5 @@ public:
 	double volume(void) const;			// объем тела вращения
 private:
 	double a;
+	const double pi = 3.141592;
 };
