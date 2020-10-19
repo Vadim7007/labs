853b5adee560263c43c230711b75cbdf818e4c75
Vadim7007 <vadim-baz@mail.ru>
2020-10-19 10:22:56 +0300

    lab3

--- 3/lib3.h
+++ 3/lib3.h
@@ -1,21 +1,23 @@
 ﻿#include "mylib.h"
 
-class Alphabet
-{
-public:
-	Alphabet();
-	Alphabet(const int k);
-	Alphabet(const char* str);
-	friend std::ostream& operator<<(std::ostream& ostream, const Alphabet& a);
-	friend std::istream& operator>>(std::istream& istream, Alphabet& a);
-	friend Alphabet& operator+=( Alphabet& a, const char& c);
-	friend Alphabet operator+(const Alphabet& a, const Alphabet& b);
-	bool is_char(const char& c) const;
-	char* coding(const char* str, int k, const char* mode) const;
+namespace ABC_class {
+	class Alphabet
+	{
+	public:
+		Alphabet();
+		Alphabet(const int k);
+		Alphabet(const char* str);
+		friend std::ostream& operator<<(std::ostream& ostream, const Alphabet& a);
+		friend std::istream& operator>>(std::istream& istream, Alphabet& a);
+		friend Alphabet& operator+=(Alphabet& a, const char& c);
+		friend Alphabet operator+(const Alphabet& a, const Alphabet& b);
+		bool is_char(const char& c) const;
+		char* coding(const char* str, int k, const char* mode) const;
 
-private:
-	int get_index(const char& c) const;
-	static const int N = 26;	// максимальный размер алфавита
-	int n;						// текущий размер алфавита
-	char abc[N];				// алфавит
+	private:
+		int get_index(const char& c) const;
+		static const int N = 26;	// максимальный размер алфавита
+		int n;						// текущий размер алфавита
+		char abc[N];				// алфавит
+	};
 };
--- 3/main.cpp
+++ 3/main.cpp
@@ -1,6 +1,34 @@
 ﻿#include "lib3.h"
 
 int main(void) {
+	int k = 0;
+	while (k != -1) {
+		system("cls");
+		std::cout << "Enter action" << std::endl;
+		std::cout << "-1 - Exit" << std::endl;
+		std::cout << " 0 - Create empty abc" << std::endl;
+		std::cout << " 1 - Create abc of length n" << std::endl;
+		std::cout << " 2 - Create abc from string" << std::endl;
+		getn(k);
+		if (k > -1 && k < 3)
+		{
+			switch (k)
+			{
+				case 0:
+					ABC_class::Alphabet a;
+					break;
+				case 1:
+					int n;
+					getn(n);
+					ABC_class::Alphabet a(n);
+					break;
+				case 2:
 
+					break;
+
+			}
+		}
+	}
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
